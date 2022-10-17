920b9f0edcb9b49c78170ee1f7592e2516c4aed1
ahdub <ahdub@DESKTOP-FRK4R38>
2022-10-17 14:51:02 -0500

    Add project files.

---
+++ Intro to Programming week 2 lab/Intro_to_Programming_week_2_lab.py
@@ -0,0 +1,20 @@
+#asking to input values for varablies
+
+inch=int(input("Please enter your how many inches: "))
+foot=int(input("Please enter your how many feet: "))
+yard=int(input("Please enter your how many yards: "))
+mile=int(input("Please enter your how many mile: "))
+
+#converting from english to metric
+
+centimeters_per_inch=float(inch*2.54)
+centimeters_per_foot=float(foot*30.48)
+meters_per_yard=float(yard*.914)
+kilometers_per_mile=(mile*1.61)
+
+#printing the now values as metric
+
+print(centimeters_per_inch, "centimeters")
+print(centimeters_per_foot, "centimeters")
+print(meters_per_yard, "meters")
+print(kilometers_per_mile, "kilometers")
