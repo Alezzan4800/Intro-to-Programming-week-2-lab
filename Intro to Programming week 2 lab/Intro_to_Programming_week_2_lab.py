faca5c4c9e6c644f3da67cfdbb820e93a0081d20
Alezzan4800 <ahdub23@gmail.com>
2022-10-18 00:03:13 -0500

    Update Intro_to_Programming_week_2_lab.py

--- Intro to Programming week 2 lab/Intro_to_Programming_week_2_lab.py
+++ Intro to Programming week 2 lab/Intro_to_Programming_week_2_lab.py
@@ -1,24 +1,27 @@
+def main():
 #asking to input values for varablies
 
-inch=int(input("Please enter your how many inches: "))
-foot=int(input("Please enter your how many feet: "))
-yard=int(input("Please enter your how many yards: "))
-mile=int(input("Please enter your how many mile: "))
+    inch=int(input("Please enter your how many inches: "))
+    foot=int(input("Please enter your how many feet: "))
+    yard=int(input("Please enter your how many yards: "))
+    mile=int(input("Please enter your how many mile: "))
 
 
 
-print("---------------------")
+    print("---------------------")
 
-#converting from imperial to metric
+    #converting from imperial to metric
 
-centimeters_per_inch=float(inch*2.54)
-centimeters_per_foot=float(foot*30.48)
-meters_per_yard=float(yard*.914)
-kilometers_per_mile=float((mile*1.61))
+    centimeters_from_inch=float(inch*2.54)
+    centimeters_from_foot=float(foot*30.48)
+    meters_from_yard=float(yard*.914)
+    kilometers_from_mile=float((mile*1.61))
 
-#printing the now values as metric
+    #printing the now values as metric
 
-print(centimeters_per_inch, "centimeters")
-print(centimeters_per_foot, "centimeters")
-print(meters_per_yard, "meters")
-print(kilometers_per_mile, "kilometers")
+    print(centimeters_from_inch, "centimeters")
+    print(centimeters_from_foot, "centimeters")
+    print(meters_from_yard, "meters")
+    print(kilometers_from_mile, "kilometers")
+
+main()
